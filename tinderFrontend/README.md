4863dee8e119ca1841b6be24dcf1148177460f23
Khalid Uris <uriskhalid@gmail.com>
2025-11-02 22:32:41 +0500

    create Body + Login + NavBar + Footer component and  install react-router-dom

--- tinderFrontend/README.md
+++ tinderFrontend/README.md
@@ -5,3 +5,16 @@
 - Install Tailwind css
 - Install daisyUi
 - Add Navbar component to app.jsx
+- Create a NavBar.jsx separate component file
+- Install react router dom
+- Create BrowserRouter > Routes > Route=/Body > RouteChildren
+- Create an Outlet in your Body Component
+- Create a footer
+
+# Body
+
+- NavBar
+- Route/ => Feed
+- Route/login => Login
+- Route/connections => Connection
+- Route/profile => Profile
--- tinderFrontend/src/App.jsx
+++ tinderFrontend/src/App.jsx
@@ -1,45 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 
 function App() {
-  
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
-  return <>
-  <div className="navbar bg-base-300">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevTinder</a>
-  </div>
-  <div className="flex-none gap-2">
-    {/* <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div> */}
-    <div className="dropdown dropdown-end mx-5">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-  <button
-  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-  Button
-</button>
-  </>  
-
+      {/* <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button> */}
+    </>
+  );
 }
 
-export default App
+export default App;
