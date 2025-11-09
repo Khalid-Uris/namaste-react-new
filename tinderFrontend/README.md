86c68609069d034eb7149932832f5b829760c4f1
Khalid Uris <uriskhalid@gmail.com>
2025-11-09 22:51:04 +0500

    fetch data from useSelector in nav bar and add component folder + base url. Also Complete 2nd Episode devTinder.

--- tinderFrontend/README.md
+++ tinderFrontend/README.md
@@ -23,6 +23,13 @@
 - Install Redux-toolkit + @reduxjs/toolkit => https://redux-toolkit.js.org/introduction/getting-started
 - configureStore => Provider => createSlice => add reducer to store
 
+# 09-11-2025
+
+- Add redux devtools in chrome
+- Login and see if your data is coming properly in the store
+- NavBar should update as soon as user logs in
+- Refactor our code to add constansts file + create a components folder
+
 # Body
 
 - NavBar
--- tinderFrontend/src/components/Login.jsx
+++ tinderFrontend/src/components/Login.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { addUser } from "./utils/userSlice";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setpassword] = useState("");
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
-      console.log(res.data);
+      // console.log(res.data);
       dispatch(addUser(res.data));
+      navigate("/");
     } catch (err) {
       console.log("Error in login:" + err);
     }
